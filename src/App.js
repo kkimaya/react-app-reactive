2af39f09196ea8dc326a2a60d3a9b6ad27c0bdf8
Kimaya Kamat <kkimaya@gmail.com>
2019-09-24 13:49:30 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './GroupList';
+import GroupEdit from './GroupEdit';
+import { CookiesProvider } from 'react-cookie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/groups');
+    const body = await response.json();
+    this.setState({ groups: body, isLoading: false });
+  }
+
+  render() {
+      return (
+
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/groups' exact={true} component={GroupList}/>
+            <Route path='/groups/:id' component={GroupEdit}/>
+          </Switch>
+        </Router>
+
+      )
+    }
 }
+
 
 export default App;
